15810b59434b30c04d06588bd8f51ee1e801422f
A Belgrave <abelgrave95@gmail.com>
2019-02-15 02:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var bill = [];
+  for (var i = 0; i < musicians.length; i++) {
+    bill[i] = musicians[i] + " plays " + instruments[i];
+  }
+
+  return bill
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  var exclamation = [];
+  while (i < facts.length) {
+    facts[i].concat("!!!");
+    //exclamation.push(facts[i] + "!!!");
+    i++;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(num){
+  let i = num;
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    i++;
+  } while (i < 15);
+
+  return love
+}
